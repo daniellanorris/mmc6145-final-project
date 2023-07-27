96e448c81fda904eb4e0a96c6c4459677a027f10
daniellanorris <daniellanorris@ufl.edu>
2023-07-26 22:34:25 -0600

    adding create-react

---
+++ final-project/pages/login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ObFormsLogin } from "./ObFormsLogin";
+import { ObFormsPassword } from "./ObFormsPassword";
+import { ObFormsUsername } from "./ObFormsUsername";
+import "./style.css";
+
+export const LoginSignup = ({ className, OBFormsLoginPropertyDefaultClassName }) => {
+  return (
+    <div className={`login-signup ${className}`}>
+      <div className="rectangle" />
+      <div className="overlap-group">
+        <img className="img" alt="Rectangle" src="rectangle-3.png" />
+        <ObFormsUsername className="OB-forms-username-instance" property1="default" />
+      </div>
+      <ObFormsPassword className="OB-forms-password-instance" property1="default" />
+      <ObFormsLogin className={OBFormsLoginPropertyDefaultClassName} property1="default" />
+      <div className="howdy-do-your-thang">howdy! do your thang.</div>
+    </div>
+  );
+};
